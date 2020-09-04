c6634c0145d61b1cb5b915e3819fe0e31fb11704
J.Moriarty <j.moriarty721@gmail.com>
2020-09-04 00:41:53 -0400

    contact page redo, other smaller edits across pages

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+//for contact page
+
+
+var button = document.getElementById("email");
+button.addEventListener('click', function() {
+  if (button.getAttribute("data-text-swap") == button.innerHTML) {
+    button.innerHTML = button.getAttribute("data-text-original");
+  } else {
+    button.setAttribute("data-text-original", button.innerHTML);
+    button.innerHTML = button.getAttribute("data-text-swap");
+  }
+}, false);
